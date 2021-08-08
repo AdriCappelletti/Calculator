37a90d4f25041f10defe290d6cdd900dddbc396f
Adriano Cappelletti <adriano.contacto@gmail.com>
2021-08-08 18:07:35 -0300

    Add Eslint

--- src/ui.js
+++ src/ui.js
@@ -1,31 +1,30 @@
-import { getClickedButton, controleButtonClick } from "./utility.js";
+import { getClickedButton, controleButtonClick } from './utility.js';
 
-const $calcBody = document.querySelector("#calc-body");
-const $calcVisor = document.querySelector("#visor-container__visor");
+const $calcBody = document.querySelector('#calc-body');
+const $calcVisor = document.querySelector('#visor-container__visor');
 
-$calcBody.addEventListener("click", (e) => {
+$calcBody.addEventListener('click', (e) => {
   controleButtonClick(getClickedButton(e));
 });
 
-document.querySelector("#delete-btn").addEventListener("click", () => {
+document.querySelector('#delete-btn').addEventListener('click', () => {
   $calcVisor.value = $calcVisor.value.slice(0, -1);
 });
 
-export const writeVisor = (operation, data) => {
-  if (operation) {
-    $calcVisor.value = "";
+document.querySelector('#reset').addEventListener('click', () => {
+  $calcVisor.value = '0';
+});
+
+export const writeVisor = (isResult, data) => {
+  if (isResult) {
+    $calcVisor.value = '';
   }
-  $calcVisor.value === "0"
+  $calcVisor.value === '0'
     ? ($calcVisor.value = data)
     : ($calcVisor.value += data);
 };
 
-export const getVisorValue = () => {
-  return $calcVisor.value;
-};
-
-export const resetVisor = document
-  .querySelector("#reset")
-  .addEventListener("click", () => {
-    $calcVisor.value="0"
-  });
+export const getVisorValue = () => $calcVisor.value;
+document.addEventListener('keypress', (e) => {
+  writeVisor(false, e.key);
+});
