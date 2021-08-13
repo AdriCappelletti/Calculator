7a446aad04549f07644dd725e6374a2af433c965
Adriano Cappelletti <73238164+AdriCappelletti@users.noreply.github.com>
2021-08-13 12:04:57 -0300

    Develop (#5)
    * Code refactor an style improvement
    
    Starts refactoring to improve code and adds button colors
    
    * Comment unnecessary border style
    
    * Merge index main into develop
    
    * Add new themes and deletes unused code Lines
    
    Finishes the theme switch functionality

--- src/ui.js
+++ src/ui.js
@@ -46,48 +46,91 @@ $switchInput.addEventListener('input', () => {
   switchTheme(theme);
 });
 
-const setTheme1 = () => {
+// theme settings
+const cssVariables = document.documentElement.style;
 
+const setTheme1 = () => {
+  cssVariables.setProperty('--main-bg', '');
+  cssVariables.setProperty('--toggle-keypad-bg', '');
+  cssVariables.setProperty('--visor-bg', '');
+  cssVariables.setProperty('--header-text-color', '');
+
+  /* Default Buttons */
+  cssVariables.setProperty('--btn-default', '');
+  cssVariables.setProperty('--btn-default-shadow', '');
+  cssVariables.setProperty('--btn-default-shadow', '');
+
+  /* Erase Buttons */
+  cssVariables.setProperty('--btn-erase', '');
+  cssVariables.setProperty('--btn-erase-shadow', '');
+
+  /* Equal Button */
+  cssVariables.setProperty('--btn-equal', '');
+  cssVariables.setProperty('--btn-equal-shadow', '');
+
+  /* Button Color */
+  cssVariables.setProperty('--btn-default-color', '');
+  cssVariables.setProperty('--btn-erase-color', '');
+  cssVariables.setProperty('--btn-equal-color', '');
 };
 
 const setTheme2 = () => {
-
-};
-
-const setTheme3 = () => {
-  document.documentElement.style.setProperty('--main-bg', 'hsl(268, 75%, 9%)');
-  document.documentElement.style.setProperty('--toggle-keypad-bg', ' hsl(268, 71%, 12%)');
-  document.documentElement.style.setProperty('--visor-bg', ' hsl(268, 71%, 12%)');
-  document.documentElement.style.setProperty('--header-text-color', ' hsl(52, 100%, 62%)');
+  cssVariables.setProperty('--main-bg', 'hsl(0, 0%, 90%)');
+  cssVariables.setProperty('--toggle-keypad-bg', ' hsl(0, 5%, 81%)');
+  cssVariables.setProperty('--visor-bg', ' hsl(0, 0%, 93%)');
+  cssVariables.setProperty('--header-text-color', 'hsl(60, 10%, 19%)');
 
   /* Default Buttons */
-  document.documentElement.style.setProperty('--btn-default', 'hsl(268, 47%, 21%)');
-  document.documentElement.style.setProperty('--btn-default-shadow', 'hsl(290, 70%, 36%)');
-  document.documentElement.style.setProperty('--btn-default-shadow', 'hsl(290, 70%, 36%)');
+  cssVariables.setProperty('--btn-default', 'hsl(45, 7%, 89%)');
+  cssVariables.setProperty('--btn-default-shadow', 'hsl(35, 11%, 61%)');
+  // cssVariables.setProperty('--btn-default-shadow', 'hsl(290, 70%, 36%)');
 
   /* Erase Buttons */
-  document.documentElement.style.setProperty('--btn-erase', 'hsl(281, 89%, 26%)');
-  document.documentElement.style.setProperty('--btn-erase-shadow', 'hsl(285, 91%, 52%)');
-
-  /* Reset Button */
-  document.documentElement.style.setProperty('--btn-reset', '#00bcd4');
-  document.documentElement.style.setProperty('--btn-reset-shadow', '#00bcd4');
+  cssVariables.setProperty('--btn-erase', 'hsl(185, 42%, 37%)');
+  cssVariables.setProperty('--btn-erase-shadow', 'hsl(185, 58%, 25%)');
 
   /* Equal Button */
-  document.documentElement.style.setProperty('--btn-equal', 'hsl(176, 100%, 44%)');
-  document.documentElement.style.setProperty('--btn-equal-shadow', 'hsl(177, 92%, 70%)');
+  cssVariables.setProperty('--btn-equal', 'hsl(25, 98%, 40%)');
+  cssVariables.setProperty('--btn-equal-shadow', ' hsl(25, 99%, 27%)');
 
   /* Button Color */
-  document.documentElement.style.setProperty('--btn-color', 'hsl(52, 100%, 62%)');
+  cssVariables.setProperty('--btn-color', 'hsl(0, 0, 100%)');
 };
-// hsl(176, 100%, 44%)
+
+// setTheme2();
+const setTheme3 = () => {
+  cssVariables.setProperty('--main-bg', 'hsl(268, 75%, 9%)');
+  cssVariables.setProperty('--toggle-keypad-bg', ' hsl(268, 71%, 12%)');
+  cssVariables.setProperty('--visor-bg', ' hsl(268, 71%, 12%)');
+  cssVariables.setProperty('--header-text-color', ' hsl(52, 100%, 62%)');
+
+  /* Default Buttons */
+  cssVariables.setProperty('--btn-default', 'hsl(268, 47%, 21%)');
+  cssVariables.setProperty('--btn-default-shadow', 'hsl(290, 70%, 36%)');
+  cssVariables.setProperty('--btn-default-shadow', 'hsl(290, 70%, 36%)');
+
+  /* Erase Buttons */
+  cssVariables.setProperty('--btn-erase', 'hsl(281, 89%, 26%)');
+  cssVariables.setProperty('--btn-erase-shadow', 'hsl(285, 91%, 52%)');
+
+  /* Equal Button */
+  cssVariables.setProperty('--btn-equal', 'hsl(176, 100%, 44%)');
+  cssVariables.setProperty('--btn-equal-shadow', 'hsl(177, 92%, 70%)');
+
+  /* Button Color */
+  cssVariables.setProperty('--btn-default-color', 'hsl(52, 100%, 62%)');
+  cssVariables.setProperty('--btn-erase-color', '');
+  cssVariables.setProperty('--btn-equal-color', 'hsl(198, 20%, 13%)');
+};
+
 const switchTheme = (theme) => {
   switch (theme) {
     case '3':
       setTheme3();
       break;
-
-    default: location.reload();
+    case '2': setTheme2();
+      break;
+    default: setTheme1();
       break;
   }
 };
